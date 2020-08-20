63aaae82b27ef1caf235fc90f8185c03d615def4
Paul Forness <50929905+ptforness@users.noreply.github.com>
2020-08-20 10:06:24 -0500

    Updated package.json

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,31 +1,27 @@
 /// <reference types="node" />
 declare module "index" {
-  export interface Metadata {
-    pageCount?: number;
-    resultCount?: number;
-  }
-  export interface Parameters {
-    pageNumber?: number;
-    sortOrder?: SortOrder;
-  }
-  export interface Publication {
-    title?: string;
-    hyperlink?: string;
-    doi?: string;
-    isEmbargoed?: boolean;
-    publicationDate?: Date;
-    embargoReleaseDate?: Date;
-  }
-  export interface Response {
-    publications: Publication[];
-    metadata: Metadata;
-  }
-  export type SortOrder = "Relevance" | "DateAscending" | "DateDescending";
-  export function getUrl(pageNumber?: number, sortOrder?: SortOrder): string;
-  export function getUrlRange(
-    startPage: number,
-    endPage: number,
-    sortOrder?: SortOrder
-  ): string[];
-  export function scrapePage(html: string | Buffer): Response;
+    export interface Metadata {
+        pageCount?: number;
+        resultCount?: number;
+    }
+    export interface Parameters {
+        pageNumber?: number;
+        sortOrder?: SortOrder;
+    }
+    export interface Publication {
+        title?: string;
+        hyperlink?: string;
+        doi?: string;
+        isEmbargoed?: boolean;
+        publicationDate?: Date;
+        embargoReleaseDate?: Date;
+    }
+    export interface Response {
+        publications: Publication[];
+        metadata: Metadata;
+    }
+    export type SortOrder = "Relevance" | "DateAscending" | "DateDescending";
+    export function getUrl(pageNumber?: number, sortOrder?: SortOrder): string;
+    export function getUrlRange(startPage: number, endPage: number, sortOrder?: SortOrder): string[];
+    export function scrapePage(html: string | Buffer): Response;
 }
