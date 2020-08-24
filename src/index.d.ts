faf6cc48c08f8d30f76b2e526a57cdee50cbae42
Paul Forness <50929905+ptforness@users.noreply.github.com>
2020-08-24 15:05:51 -0500

    Added git/npm hooks, updated API

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,36 @@
 /// <reference types="node" />
+declare module "url" {
+  import { NsfParSortOrder as urlBuilder } from "index";
+  export function get(pageNumber?: number, sortOrder?: urlBuilder): string;
+  export function getRange(
+    startPage: number,
+    endPage: number,
+    sortOrder?: urlBuilder
+  ): string[];
+}
+declare module "request" {
+  export function request(url: string): Promise<string>;
+}
+declare module "processing" {
+  export function processDoi(doi: string): string | undefined;
+  export function processReleaseDate(releaseDateText: string): Date | undefined;
+}
+declare module "scrape" {
+  import { NsfParMetadata, NsfParPublication, NsfParResponse } from "index";
+  export function scrapeResponse(html: string | Buffer): NsfParResponse;
+  export function getMetadata(response: CheerioStatic): NsfParMetadata;
+  export function getPublications(response: CheerioStatic): NsfParPublication[];
+}
 declare module "index" {
-  export interface Metadata {
+  export interface NsfParMetadata {
     pageCount?: number;
     resultCount?: number;
   }
-  export interface Parameters {
+  export interface NsfParParameters {
     pageNumber?: number;
-    sortOrder?: SortOrder;
+    sortOrder?: NsfParSortOrder;
   }
-  export interface Publication {
+  export interface NsfParPublication {
     title?: string;
     hyperlink?: string;
     doi?: string;
@@ -16,16 +38,16 @@ declare module "index" {
     publicationDate?: Date;
     embargoReleaseDate?: Date;
   }
-  export interface Response {
-    publications: Publication[];
-    metadata: Metadata;
+  export interface NsfParResponse {
+    publications: NsfParPublication[];
+    metadata: NsfParMetadata;
   }
-  export type SortOrder = "Relevance" | "DateAscending" | "DateDescending";
-  export function getUrl(pageNumber?: number, sortOrder?: SortOrder): string;
-  export function getUrlRange(
-    startPage: number,
-    endPage: number,
-    sortOrder?: SortOrder
-  ): string[];
-  export function scrapePage(html: string | Buffer): Response;
+  export type NsfParSortOrder =
+    | "Relevance"
+    | "DateAscending"
+    | "DateDescending";
+  export function getPage(
+    pageNumber?: number,
+    sortOrder?: NsfParSortOrder
+  ): Promise<NsfParResponse>;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import * as cheerio from "cheerio";
+import { request } from "./request";
+import { scrapeResponse } from "./scrape";
+import * as urlBuilder from "./url";
 
-export interface Metadata {
+export interface NsfParMetadata {
   pageCount?: number;
   resultCount?: number;
 }
-export interface Parameters {
+export interface NsfParParameters {
   pageNumber?: number;
-  sortOrder?: SortOrder;
+  sortOrder?: NsfParSortOrder;
 }
-export interface Publication {
+export interface NsfParPublication {
   title?: string;
   hyperlink?: string;
   doi?: string;
@@ -16,144 +18,17 @@ export interface Publication {
   publicationDate?: Date;
   embargoReleaseDate?: Date;
 }
-export interface Response {
-  publications: Publication[];
-  metadata: Metadata;
+export interface NsfParResponse {
+  publications: NsfParPublication[];
+  metadata: NsfParMetadata;
 }
-export type SortOrder = "Relevance" | "DateAscending" | "DateDescending";
+export type NsfParSortOrder = "Relevance" | "DateAscending" | "DateDescending";
 
-export function getUrl(
+export async function getPage(
   pageNumber = 1,
-  sortOrder: SortOrder = "Relevance"
-): string {
-  let url = "https://par.nsf.gov/search";
-  if (pageNumber > 1) {
-    url += `/page:${pageNumber}`;
-  }
-
-  switch (sortOrder) {
-    case "DateAscending":
-      url += "/sort:publication_date%20asc";
-      break;
-    case "DateDescending":
-      url += "/sort:publication_date%20desc";
-      break;
-  }
-  return url;
-}
-
-export function getUrlRange(
-  startPage: number,
-  endPage: number,
-  sortOrder?: SortOrder
-): string[] {
-  const urls: string[] = [];
-
-  for (let pageNumber = startPage; pageNumber <= endPage; pageNumber++) {
-    urls.push(getUrl(pageNumber, sortOrder));
-  }
-  return urls;
-}
-
-/**
- * Scrapes publication data from an empty query against the NSF PAR.
- * @remarks Only empty queries are supported, as result accuracy with any non-sort parameter has been low in my experience.
- * @param data The raw response data to be scraped
- * @returns The processed publication data and search metadata for the given page number
- */
-export function scrapePage(html: string | Buffer): Response {
-  const data: CheerioStatic = cheerio.load(html);
-  return {
-    metadata: getMetadata(data),
-    publications: getPublications(data),
-  };
-}
-
-/**
- * @ignore
- * @param response
- */
-function getMetadata(response: CheerioStatic): Metadata {
-  const pageCount = Number(response(".paging-max").first().text());
-  const resultCount = Number(response("#item-count-everything").text());
-  return {
-    pageCount,
-    resultCount,
-  };
-}
-
-/**
- * @ignore
- * @param response
- */
-function getPublications(response: CheerioStatic): Publication[] {
-  const publications: Publication[] = [];
-  const publicationData: Cheerio = response(".article , .item , .document");
-  publicationData.each(function () {
-    const publicationDate: Date = new Date(
-      response(this).find("time[itemprop=datePublished]").attr("datetime")
-    );
-
-    const isEmbargoed: boolean = response(this)
-      .find("span.reader-count")
-      .text()
-      .includes("Free, publicly-accessible full text available");
-
-    const embargoReleaseDate: Date = parseReleaseDate(
-      response(this).find("span.reader-count").text()
-    );
-
-    publications.push({
-      title: response(this).find("span[itemprop=name]").text(),
-      hyperlink: response(this).find("a[itemprop=url]").attr("href"),
-      doi: cleanDOI(response(this).find("a.misc , .doi-link").attr("href")),
-      isEmbargoed,
-      publicationDate,
-      embargoReleaseDate,
-    });
-  });
-  return publications;
-}
-
-/**
- * @ignore
- * @param doi
- */
-function cleanDOI(doi: string): string | undefined {
-  if (doi == null) return undefined;
-
-  const forbiddenSubstrings: (string | RegExp)[] = [
-    /https?:\/\/dx.doi.org\//,
-    /https?:\/\/doi.org\//,
-    /doi:\w?/,
-    /I:\w?/,
-    /https:\/\/pubs.acs.org\/doi\//,
-  ];
-
-  for (const i of forbiddenSubstrings) {
-    doi = doi.replace(i, "");
-  }
-
-  return doi;
-}
-
-/**
- * @ignore
- * @param releaseDateText
- */
-function parseReleaseDate(releaseDateText: string): Date | undefined {
-  const monthRegExp = /(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[Mm]ar(?:ch)?|[Aa]pr(?:il)?|[Mm]ay|[Jj]un(?:e)?|[Jj]ul(?:y)?|[Aa]ug(?:ust)?|[Ss]ep(?:tember)?|[Oo]ct(?:ober)?|[Nn]ov(?:ember)?|[Dd]ec(?:ember)?)/;
-  const dayRegExp = /(?:(?:[1-2][0-9])|(?:3[0-1])|(?:0?[1-9]))/;
-  const yearRegExp = /(?:\d{4}|\d{2})/;
-  const finalRegExp = new RegExp(
-    monthRegExp.source +
-      /\s/.source +
-      dayRegExp.source +
-      /,\s/.source +
-      yearRegExp.source
-  );
-  const matches: RegExpMatchArray = releaseDateText.match(finalRegExp);
-  return matches == null || matches.length === 0
-    ? undefined
-    : new Date(matches[0]);
+  sortOrder: NsfParSortOrder = "Relevance"
+): Promise<NsfParResponse> {
+  const url = urlBuilder.get(pageNumber, sortOrder);
+  const data = await request(url);
+  return scrapeResponse(data);
 }
