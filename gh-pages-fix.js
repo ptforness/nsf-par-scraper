d8aee844e080ede73f6b4cd189e9bc2f8857b098
Paul Forness <50929905+ptforness@users.noreply.github.com>
2020-08-24 16:55:19 -0500

    Renaming fix for typedoc 404 errors

--- gh-pages-fix.js
+++ gh-pages-fix.js
@@ -1,4 +1,19 @@
 const fs = require('fs');
-const filePath = './docs/config.yml';
-const data = "include:\n- \"_*_.html\"\n- \"_*_.*.html\"";
-fs.writeFileSync(filePath, data);
+const path = require('path');
+//const filePath = './docs/config.yml';
+//const data = "include:\n- \"_*_.html\"\n- \"_*_.*.html\"";
+//fs.writeFileSync(filePath, data);
+
+const documentationFolder = './docs/';
+const interfaces = path.join(documentationFolder, 'interfaces/');
+const modules = path.join(documentationFolder, 'modules/');
+rename(interfaces);
+rename(modules);
+
+function rename(folder) {
+    fs.readdirSync(folder).forEach(file => {
+        if(file.startsWith('_')) {
+            fs.renameSync(path.join(folder, file), path.join(folder, file.substring(1)));
+        }
+    })
+}
